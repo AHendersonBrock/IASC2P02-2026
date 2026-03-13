de09fb232a7944670f8692ae77e1b66fc2d0cd27
Adam Henderson <ahenderson2@brocku.ca>
2026-03-12 20:21:28 -0400

    Data Viz bonus

---
+++ Assignment 2/script.js
@@ -0,0 +1,403 @@
+import * as THREE from "three"
+import * as dat from "lil-gui"
+import { OrbitControls } from "OrbitControls"
+
+let loader = new THREE.FileLoader();
+
+/********** 
+ ** SETUP **
+***********/
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    aspectRatio: window.innerWidth / window.innerHeight
+}
+
+// Resizing
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+    sizes.aspectRatio = window.innerWidth / window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.aspectRatio
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/***********
+ ** SCENE **
+ ***********/
+//Canvas
+const canvas = document.querySelector('.webgl')
+
+let term1ID = document.getElementById("term1")
+let term2ID = document.getElementById("term2")
+let term3ID = document.getElementById("term3")
+
+const headID = document.querySelector("h1")
+headID.style.color = new THREE.Color("black")
+
+//Scene
+const scene = new THREE.Scene()
+scene.background = new THREE.Color('grey')
+
+//Camera
+const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.aspectRatio,
+    0.1,
+    100
+)
+scene.add(camera)
+camera.position.set(0, 12, -20)
+
+
+//Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+})
+renderer.setSize(sizes.width, sizes.height)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/***********
+** LIGHTS **
+************/
+
+// Directional Light
+const directionalLight = new THREE.DirectionalLight(0x404040, 100)
+scene.add(directionalLight)
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
+/************
+ ** MESHES **
+ ************/
+// Cube Geomtry
+const cubeGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
+const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 16)
+const diamondGeometry = new THREE.SphereGeometry(0.5, 0.5, 0.5)
+const torusgeometry = new THREE.TorusKnotGeometry(0.5, 0.2, 100, 16);
+let objectList = [];
+const drawCube = (height, color) => {
+    // Create cube material
+    const material = new THREE.MeshLambertMaterial({
+        color: new THREE.Color(color),
+        emissive: new THREE.Color(color),
+        wireframe: true
+    })
+
+    // Create cube
+    const cube = new THREE.Mesh(cubeGeometry, material)
+
+    // Position cube
+    cube.position.x = (Math.random() - 0.5) * uiObject.spread
+    cube.position.z = (Math.random() - 0.5) * uiObject.spread
+    cube.position.y = height - 10
+
+    // Randomize cube rotation
+    cube.rotation.x = Math.random() * 2 * Math.PI
+    cube.rotation.z = Math.random() * 2 * Math.PI
+    cube.rotation.y = Math.random() * 2 * Math.PI
+
+    // Add cube to scene
+    scene.add(cube)
+    objectList.push(cube)
+}
+const drawSphere = (height, color) => {
+    // Create cube material
+    const material = new THREE.MeshLambertMaterial({
+        color: new THREE.Color(color),
+        emissive: new THREE.Color(color)
+    })
+
+    // Create cube
+    const sphere = new THREE.Mesh(sphereGeometry, material)
+
+    // Position cube
+    sphere.position.x = (Math.random() - 0.5) * uiObject.spread
+    sphere.position.z = (Math.random() - 0.5) * uiObject.spread
+    sphere.position.y = height - 10
+
+    // Add cube to scene
+    scene.add(sphere)
+    objectList.push(sphere)
+}
+const drawDiamond = (height, color) => {
+    // Create cube material
+    const material = new THREE.MeshLambertMaterial({
+        color: new THREE.Color(color),
+        emissive: new THREE.Color(color)
+    })
+
+    // Create cube
+    const sphere = new THREE.Mesh(diamondGeometry, material)
+
+    // Position cube
+    sphere.position.x = (Math.random() - 0.5) * uiObject.spread
+    sphere.position.z = (Math.random() - 0.5) * uiObject.spread
+    sphere.position.y = height - 10
+
+    // Randomize cube rotation
+    sphere.rotation.x = Math.random() * 2 * Math.PI
+    sphere.rotation.z = Math.random() * 2 * Math.PI
+    sphere.rotation.y = Math.random() * 2 * Math.PI
+
+    // Add cube to scene
+    scene.add(sphere)
+    objectList.push(sphere)
+}
+const drawTorus = (height, color) => {
+    // Create cube material
+    const material = new THREE.MeshLambertMaterial({
+        color: new THREE.Color(color),
+        emissive: new THREE.Color(color)
+    })
+
+    // Create cube
+    const sphere = new THREE.Mesh(torusgeometry, material)
+
+    // Position cube
+    sphere.position.x = (Math.random() - 0.5) * uiObject.spread
+    sphere.position.z = (Math.random() - 0.5) * uiObject.spread
+    sphere.position.y = height - 10
+
+    // Randomize cube rotation
+    sphere.rotation.x = Math.random() * 2 * Math.PI
+    sphere.rotation.z = Math.random() * 2 * Math.PI
+    sphere.rotation.y = Math.random() * 2 * Math.PI
+
+    // Add cube to scene
+    scene.add(sphere)
+    objectList.push(sphere)
+}
+
+const removeObjects = () => {
+    objectList.forEach(function (object) {
+        scene.remove(object)
+    })
+}
+
+//drawCube(0, 'pink')
+//drawCube(5, 'pink')
+
+
+
+
+
+/********
+ ** UI **
+ ********/
+// UI
+const ui = new dat.GUI()
+
+const uiObject =
+{
+    amount: 1,
+    spread: 10,
+    layerSpacing: 100,
+    type: "cube",
+    term1: "downtime",
+    term1Color: new THREE.Color(),
+    term2: "roll",
+    term2Color: new THREE.Color(),
+    term3: "bonus",
+    term3Color: new THREE.Color()
+}
+
+const wordFolder = ui.addFolder('Words')
+
+wordFolder
+    .add(uiObject, "term1")
+    .name("Search Term 1")
+    .onChange(() => {
+        removeObjects()
+        updateSearchTerms()
+    })
+
+wordFolder
+    .add(uiObject, "term2")
+    .name("Search Term 2")
+    .onChange(() => {
+        removeObjects()
+        updateSearchTerms()
+    })
+
+wordFolder
+    .add(uiObject, "term3")
+    .name("Search Term 3")
+    .onChange(() => {
+        removeObjects()
+        updateSearchTerms()
+    })
+wordFolder
+    .addColor(uiObject, "term1Color")
+    .name("Term 1 Color")
+    .onChange(() => {
+        removeObjects()
+        updateSearchTerms()
+    })
+wordFolder
+    .addColor(uiObject, "term2Color")
+    .name("Term 2 Color")
+    .onChange(() => {
+        removeObjects()
+        updateSearchTerms()
+    })
+wordFolder
+    .addColor(uiObject, "term3Color")
+    .name("Term 3 Color")
+    .onChange(() => {
+        removeObjects()
+        updateSearchTerms()
+    })
+
+
+
+const cubeFolder = ui.addFolder('Cubes')
+
+cubeFolder
+    .add(uiObject, 'amount')
+    .min(1)
+    .max(500)
+    .step(1)
+    .name('Amount of Objects')
+    .onChange(() => {
+        removeObjects()
+        updateSearchTerms()
+    })
+
+cubeFolder
+    .add(uiObject, 'type', ["cube", "sphere", "diamond", "torus"])
+    .name('Object Type')
+    .onChange(() => {
+        removeObjects()
+        updateSearchTerms()
+    })
+
+cubeFolder
+    .add(uiObject, 'spread')
+    .name('Object Spread')
+    .min(1)
+    .max(100)
+    .step(1)
+    .onChange(() => {
+        removeObjects()
+        updateSearchTerms()
+    })
+
+cubeFolder
+    .add(uiObject, 'layerSpacing')
+    .min(100)
+    .max(200)
+    .step(1)
+    .name('Space Between Layers')
+    .onChange(() => {
+        removeObjects()
+        updateSearchTerms()
+    })
+
+
+/******************
+** TEXT ANALYSIS **
+*******************/
+let sourceText
+let parsedText, tokenizedText
+loader.load("Specialty Facilities.txt", function (text) {
+    sourceText = text;
+    tokenizeSourceText()
+    updateSearchTerms()
+});
+// Parse and Toknize sourceText
+const tokenizeSourceText = () => {
+    // Strip periods and downcase sourceText
+    parsedText = sourceText.replaceAll(".", "").toLowerCase()
+
+    // Tokenize text
+    tokenizedText = parsedText.split(/[^\w']+/)
+
+}
+
+// Find searchTerm in tokenizedText
+const findSearchTermInTokenizedText = (term, color) => {
+    // Use a for loop to go through the toknizedText array
+    for (let i = 0; i < tokenizedText.length; i++) {
+        // If tokenized Tex[i] matches our searchTerm, then we draw a cube
+        if (tokenizedText[i] === term) {
+            // convert i into height, which is a value between 0 and 20
+            const height = (uiObject.layerSpacing / tokenizedText.length) * i * 0.2
+
+            // call drawCube function 100 times using converted height value
+            for (let a = 0; a < uiObject.amount; a++) {
+                if (uiObject.type == "cube")
+                    drawCube(height, color)
+                else if (uiObject.type == "sphere")
+                    drawSphere(height, color)
+                else if (uiObject.type == "diamond")
+                    drawDiamond(height, color)
+                else if (uiObject.type == "torus")
+                    drawTorus(height, color)
+            }
+        }
+    }
+}
+
+const updateSearchTerms = () => {
+    findSearchTermInTokenizedText(uiObject.term1, uiObject.term1Color)
+    findSearchTermInTokenizedText(uiObject.term2, uiObject.term2Color)
+    findSearchTermInTokenizedText(uiObject.term3, uiObject.term3Color)
+    term1ID.innerHTML = uiObject.term1;
+    term1ID.style.color = "#"+uiObject.term1Color.getHexString();
+    console.log(uiObject.term1Color.getHexString())
+    console.log(term1ID.style.color)
+    term2ID.innerHTML = uiObject.term2;
+    term2ID.style.color = "#"+uiObject.term2Color.getHexString();
+    term3ID.innerHTML = uiObject.term3;
+    term3ID.style.color = "#"+uiObject.term3Color.getHexString();
+}
+
+
+
+
+
+
+/********************
+ ** ANIMATION LOOP ** 
+ ********************/
+const clock = new THREE.Clock()
+
+const animation = () => {
+    // Return elapsedtime
+    const elapsedTime = clock.getElapsedTime()
+
+    objectList.forEach((object) => {
+        object.position.y += Math.sin(elapsedTime) * 0.01;
+        object.rotation.x += 0.05
+    })
+
+    // Update OrbitControls
+    controls.update()
+
+    //renderer
+    renderer.render(scene, camera)
+
+    //request next frame
+    window.requestAnimationFrame(animation)
+
+}
+
+animation()
+
+
+
+
+
+
